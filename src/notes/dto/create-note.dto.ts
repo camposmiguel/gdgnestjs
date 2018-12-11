37354fd41ff9c2880741e49771ece4029c94f3b2
Miguel CR <camposmiguel@gmail.com>
2018-12-11 22:29:03 +0100

    Controller Notas

---
+++ src/notes/dto/create-note.dto.ts
@@ -0,0 +1,5 @@
+
+export class CreateNoteDto {
+    titulo: string;
+    favorita: boolean;
+}
--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,6 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Put, Body, Post, Delete } from '@nestjs/common';
+import { CreateNoteDto } from './dto/create-note.dto';
 
 @Controller('notes')
 export class NotesController {
-    // ...
+
+    @Get('/all')
+    findAll() {
+        return 'Devuelve JSON con listado de notas';
+    }
+
+    @Get(':id')
+    findOne(@Param('id') idNota) {
+        return 'Devuelve JSON de una nota seleccionada por ID';
+    }
+
+    @Post()
+    create(@Body() createNotaDto: CreateNoteDto) {
+        return 'Crea una nota con los datos del Body';
+    }
+
+    @Put(':id')
+    update(@Param('id') idNota, @Body() updateNotaDto: CreateNoteDto) {
+        return 'Actualiza los datos recibidos del Body de la nota con ID';
+    }
+
+    @Delete(':id')
+    remove(@Param('id') idNota) {
+        return 'Eliminada la nota con idNota';
+    }
+
 }
