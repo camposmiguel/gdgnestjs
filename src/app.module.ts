e49ec7edb9e225736d31b48eefa300d818d3811b
Miguel CR <camposmiguel@gmail.com>
2018-12-12 16:49:27 +0100

    Middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesController } from './notes/notes.controller';
 import { NoteService } from './notes/note.service';
 import { DatabaseModule } from './database/database.module';
 import { noteProviders } from 'notes/note.providers';
+import { LoggerMiddleware } from 'middleware/logger.middleware';
 
 @Module({
   imports: [DatabaseModule, DatabaseModule],
   controllers: [AppController, NotesController],
   providers: [AppService, NoteService, ...noteProviders],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('/');
+  }
+}
